9a5a62854eece098391ebe570ce8aceff3a77aca
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2024-02-05 07:40:15 -0600

    make it better

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,12 @@
 'use client';
 import { useEffect } from 'react';
-//import '@/styles/globals.css';
-//import 'bootstrap/dist/css/bootstrap.min.css';
-//import '@/styles/cards.css';
-//import '@/styles/form.css';
-//import '@/styles/hero.css';
-//import '@/styles/modal.css';
-//import '@fortawesome/fontawesome-svg-core/styles.css';
+import '@/styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@/styles/cards.css';
+import '@/styles/form.css';
+import '@/styles/hero.css';
+import '@/styles/modal.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 
 export default function RootLayout({ children }) {
   useEffect(() => {
