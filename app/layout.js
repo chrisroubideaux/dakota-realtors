0c3dbc4218f5079c4d0fdc746505a2ff4942b2ec
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2024-02-05 07:10:11 -0600

    make it better

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect } from 'react';
-import '@/styles/globals.css';
-
+//import '@/styles/globals.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 //import '@/styles/cards.css';
 //import '@/styles/form.css';
 //import '@/styles/hero.css';
