1e30cd2f4789af19de1618b654e85337ce52f047
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2024-02-05 21:21:38 -0600

    make it better

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,9 @@
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/globals.css';
-import '@/styles/cards.css';
-import '@/styles/form.css';
-import '@/styles/hero.css';
-import '@/styles/modal.css';
+//import '@/styles/cards.css';
+//import '@/styles/form.css';
+//import '@/styles/hero.css';
+//import '@/styles/modal.css';
 import BootstrapClient from '@/components/utils/BootstrapClient';
 
 export default function RootLayout({ children }) {
