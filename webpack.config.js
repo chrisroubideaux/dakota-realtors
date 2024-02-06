14ca5c3d16135e3b05f3abe2efeaf55c3ccd908d
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2024-02-06 16:13:06 -0600

    make it better

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // ...
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  // ...
+};
