83716fd3fa9a950bc40114c9314d54697fb80360
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2024-02-11 09:06:54 -0600

    Update Maps.js

--- components/misc/Maps.js
+++ components/misc/Maps.js
@@ -18,7 +18,7 @@ export default function SimpleMap() {
       style={{ height: '50vh', width: '100%' }}
     >
       <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyByGr3iMNbXEJCBZhv5N5Y5cuBCkMBtvYE' }}
+        bootstrapURLKeys={{ key: '' }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
       >
