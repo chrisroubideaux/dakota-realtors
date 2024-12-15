1467519ca5f10ab4582b50a951af502392f5e5b9
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2024-12-14 22:45:32 -0600

    updated all api endpoints for production

--- app/apartments/page.js
+++ app/apartments/page.js
@@ -25,7 +25,7 @@ const Apartments = () => {
 
     // Fetch apartments data
     axios
-      .get(' http://dakota-realtors.duckdns.org/apartments')
+      .get('dakota-realtors.duckdns.org/apartments')
       .then((response) => {
         setApartments(response.data);
       })
